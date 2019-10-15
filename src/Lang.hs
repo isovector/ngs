d6b835fd100dc5bb44b48f01a38fac836b341cd9
Vincent Ambo <tazjin@google.com>
2019-10-15 20:11:51 +0100

    refactor(Lang): Remove 'hash' function from LanguageAdapter
    Instead of forcing implementors to provide language-specific hashing
    modes, ensure that an AST serialisation to a ByteString is possible.
    
    This puts control over the hashes into NGS' domain, which is a much
    more useful place for them to be.

--- src/Lang.hs
+++ src/Lang.hs
@@ -1,19 +1,21 @@
 module Lang where
 
+import Data.ByteString (ByteString)
 import Polysemy
 import Sem.Metadata
 import Types
 
+class Serialisable t where
+  serialise :: t -> ByteString
+  deserialise :: ByteString -> Maybe t -- billy is a Just Boi
 
 -- Making languages pluggable
-class LanguageAdapter l where
+class Serialisable (AST l) => LanguageAdapter l where
   data AST l
 
-  -- impure angry IO land lalala
-  normalise    :: Member Metadata r => SourceCode -> Sem r [AST l]
-  serialise    :: Member Metadata r => AST l -> Sem r SourceCode
+  -- impure angry IO land gorilla lalala
+  normalise :: Member Metadata r => SourceCode -> Sem r [AST l]
+  render :: Member Metadata r => AST l -> Sem r SourceCode
 
   -- clean pure world, mhmm
-  hash         :: AST l -> Hash
   dependencies :: AST l -> [Hash]
-
